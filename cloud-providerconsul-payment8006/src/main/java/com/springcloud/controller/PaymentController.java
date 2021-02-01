6a90db53f351f2bf323fbfc7f7cd26a070312c44
zhangdy <1553230605@qq.com>
2021-02-01 17:55:18 +0800

    将提供方服务注册进consul中

---
+++ cloud-providerconsul-payment8006/src/main/java/com/springcloud/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @ Author     ：Psyduckzzzz
+ * @ Date       ：Created in 2021/2/1
+ */
+
+@RestController
+@Slf4j
+@RequestMapping("payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/consul")
+    public String paymentzk() {
+        return "springcloud with consul:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
