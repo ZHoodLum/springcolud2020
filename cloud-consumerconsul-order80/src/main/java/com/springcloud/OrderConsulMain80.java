7773f417c917f10e6d38216b4c618d0e464f48e2
zhangdy <1553230605@qq.com>
2021-02-02 09:20:01 +0800

    将消费方服务注册进consul中 并且可以调用提供方payment8006

---
+++ cloud-consumerconsul-order80/src/main/java/com/springcloud/OrderConsulMain80.java
@@ -0,0 +1,19 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ Author     ：Psyduckzzzz
+ * @ Date       ：Created in 2021/2/1
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+          SpringApplication.run(OrderConsulMain80.class, args);
+        System.out.println("访问80地址：  http://localhost/consumer/payment/consul ");
+    }
+}
---
+++ cloud-consumerconsul-order80/src/main/java/com/springcloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @ Author     ：Psyduckzzzz
+ * @ Date       ：Created in 2021/2/1
+ */
+
+@RestController
+@Slf4j
+@RequestMapping("/consumer")
+public class OrderController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @GetMapping("/payment/consul")
+    public String paymentInfo() {
+        log.info("调用订单模块方法：payment/consul");
+        return restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+    }
+}
