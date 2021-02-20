18161f0cd7d167daf56516463db5a2cfee2733c9
zhangdy <1553230605@qq.com>
2021-02-20 18:00:56 +0800

    新增配置管理中心 config客户端 客户端可直接访问服务端的配置

--- cloud-config-client-3355/src/main/java/com/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/springcloud/ConfigClientMain3355.java
@@ -2,18 +2,16 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Author:Psyduckzzzz
  * @Date:Created in 2021/2/20
- * @Description:配置中心服务端config配置中心
+ * @Description:配置中心客户端config配置中心
  */
 
-@EnableConfigServer
 @SpringBootApplication
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
     public static void main(String[] args) {
-          SpringApplication.run(ConfigCenterMain3344.class, args);
-     }
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
 }
---
+++ cloud-config-client-3355/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:Psyduckzzzz
+ * @Date:Created in 2021/2/20
+ * @Description:
+ */
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
