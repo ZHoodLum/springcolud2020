112eacb00094b0673a36c73c68ee17cf5cc71134
zhangdy <1553230605@qq.com>
2021-02-22 14:07:31 +0800

    新增服务总线BUS  用于管控配置中心 作用于动态刷新配置

---
+++ cloud-config-client-3366/src/main/java/com/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:Psyduckzzzz
+ * @Date:Created in 2021/2/20
+ * @Description:配置中心客户端config配置中心
+ */
+
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3366.class,args);
+    }
+}
