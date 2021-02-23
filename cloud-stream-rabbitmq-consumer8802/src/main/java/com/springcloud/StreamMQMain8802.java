1174f2c3ddfca5a165a3fe0d98d5bc979fce76c4
zhangdy <1553230605@qq.com>
2021-02-23 09:26:28 +0800

    新增消息驱动之消费者8002

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/springcloud/StreamMQMain8802.java
@@ -0,0 +1,18 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:Psyduckzzzz
+ * @Date:Created in 2021/2/22
+ * @Description:
+ */
+
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+        System.out.println("启动服务：StreamMQMain8802");
+    }
+}
