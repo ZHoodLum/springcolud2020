b86e2fc1c5cfa4c2275af5c4e280291ac171e0ba
zhangdy <1553230605@qq.com>
2021-03-05 17:38:21 +0800

    新增消费方84

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/springcloud/OrderNacosMain84.java
@@ -0,0 +1,21 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author:Psyduckzzzz
+ * @Date:Created in 2021/3/5
+ * @Description:
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class, args);
+    }
+}
